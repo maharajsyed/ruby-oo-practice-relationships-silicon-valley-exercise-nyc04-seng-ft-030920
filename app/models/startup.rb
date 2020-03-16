f8a50f0ec8befeade8e5290b2d0d309bb1b6e019
github username <github email address>
2020-03-16 01:45:39 -0400

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,36 @@
 class Startup
+        @@all = []
+        attr_accessor :name, :domain
+        attr_reader :founder
+        def initialize(name, founder, domain)
+            @name = name
+            @founder = founder
+            @domain = domain
+            @@all << self
+        end
+        
+        def pivot(domain, name)
+            @domain = domain
+            @name = name
+        end
+        
+        def self.all
+            @@all
+        end
+        
+        def self.find_by_founder(founder)
+            self.all.find do |startup|
+                startup.founder == founder
+                return startup
+            end
+        end
 
-end
+        def self.domains
+            #should return an array of all of the different startup domains
+            domain_array = []
+                all.map do |startup|
+                domain_array << startup.domain
+            end
+            domain_array
+        end
+    end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,10 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+bloomberg = VentureCapitalist.new(“Bloomberg”, 65_000_000_000)
+marc = VentureCapitalist.new(“Marc”, 500_000_000)
+pollack = VentureCapitalist.new(“Pollack”, 2_000_000_000)
+VentureCapitalist.tres_commas_club
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
